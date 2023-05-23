cfe3250a4bb7932a7bcfd6e9e7cc3cce88e57802
Fernando Nicolas Zelarayan <98678489+NicolasZelarayan@users.noreply.github.com>
2023-05-23 01:43:30 +0000

    cambios react

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import Heder from "./components/layout/Heder";
+import Pruebita from "./components/Home/Pruebita";
+import Footer from "./components/layout/Footer";
+//import Eror from "./components/pages/Eror";
+import Contac from "./components/pages/Contac";
+
+
+function App() {
+  return (
+    <>
+       <Heder/>
+      <Pruebita />
+      <Contac/>
+      <Footer/>
+
+    </>
+  );
+}
+
+export default App;
